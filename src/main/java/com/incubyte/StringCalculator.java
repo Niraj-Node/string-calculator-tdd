5183c3c38288f906e1e3de27e901567a1470661b
Niraj-Node <nirajaghera999@gmail.com>
2025-07-04 23:05:41 +0530

    feat: return number when single number is provided

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -2,6 +2,7 @@ package com.incubyte;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if (numbers.isEmpty()) return 0;
+        return Integer.parseInt(numbers);
     }
 }
