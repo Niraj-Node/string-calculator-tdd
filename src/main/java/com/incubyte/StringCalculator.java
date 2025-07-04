73f112bc1ea6dd891be5e37b1d27634d1b1fa483
Niraj-Node <nirajaghera999@gmail.com>
2025-07-04 23:21:32 +0530

    feat: return sum of two comma-separated numbers

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -3,6 +3,8 @@ package com.incubyte;
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
-        return Integer.parseInt(numbers);
+        if (numbers.length() == 1) return Integer.parseInt(numbers);
+        String[] parts = numbers.split(",");
+        return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
     }
 }
