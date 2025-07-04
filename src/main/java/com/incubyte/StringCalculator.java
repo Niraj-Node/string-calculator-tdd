d4d6c538be5a0254d848a83dded43716c4ca9a17
Niraj-Node <nirajaghera999@gmail.com>
2025-07-04 23:26:15 +0530

    feat: support sum of any number of comma-separated integers

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -4,7 +4,12 @@ public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
         if (numbers.length() == 1) return Integer.parseInt(numbers);
+
         String[] parts = numbers.split(",");
-        return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
+        int sum = 0;
+        for (String num : parts) {
+            sum += Integer.parseInt(num);
+        }
+        return sum;
     }
 }
