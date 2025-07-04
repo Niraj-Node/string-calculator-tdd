388a661f4d68b82360febac1842b7669a48e3033
Niraj-Node <nirajaghera999@gmail.com>
2025-07-04 23:37:07 +0530

    test: should support custom delimiter specified at start

--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -31,4 +31,9 @@ public class StringCalculatorTest {
         assertEquals(6, calculator.add("1\n2,3"));
     }
 
+    @Test
+    void shouldSupportCustomDelimiterSpecifiedAtStart() {
+        assertEquals(3, calculator.add("//;\n1;2"));
+    }
+
 }
