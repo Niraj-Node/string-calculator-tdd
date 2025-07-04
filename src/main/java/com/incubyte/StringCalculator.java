bcc6f7c0261acf00aac6a19e1130c4887db00f37
Niraj-Node <nirajaghera999@gmail.com>
2025-07-05 00:14:05 +0530

    feat: support custom delimiter specified with //delimiter\n syntax

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -5,7 +5,16 @@ import java.util.Arrays;
 public class StringCalculator {
     public int add(String numbers) {
         if (numbers.isEmpty()) return 0;
-        String[] parts = numbers.split("[,\n]");
+
+        String delimiter = "[,\n]";
+
+        if (numbers.startsWith("//")) {
+            int delimiterEndIndex = numbers.indexOf("\n");
+            delimiter = numbers.substring(2, delimiterEndIndex);
+            numbers = numbers.substring(delimiterEndIndex + 1);
+        }
+
+        String[] parts = numbers.split(delimiter);
         return Arrays.stream(parts)
                 .map(String::trim)
                 .mapToInt(Integer::parseInt)
