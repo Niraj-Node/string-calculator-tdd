27504427472430bfaf2611ad0aaf9226a2128d9c
Niraj-Node <nirajaghera999@gmail.com>
2025-07-05 08:02:11 +0530

    test: add failing test for custom delimiter with any length

--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -94,4 +94,9 @@ public class StringCalculatorTest {
     void shouldIncludeNumberEqualToMaxAllowed() {
         assertEquals(1002, calculator.add("2,1000"));
     }
+
+    @Test
+    void shouldSupportCustomDelimiterOfAnyLength() {
+        assertEquals(6, calculator.add("//[***]\n1***2***3"));
+    }
 }
